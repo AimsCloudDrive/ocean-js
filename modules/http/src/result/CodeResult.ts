2bc44782a37a43d18196c3f5ec6f48d957aab614
AimsCloudDrive <i79401045@126.com>
2025-06-25 22:51:42 +0800

    .

---
+++ modules/http/src/result/CodeResult.ts
@@ -0,0 +1,50 @@
+import { Nullable } from "@ocean/common";
+
+class CodeResultConstructor<
+  C extends number = number,
+  Payload extends { [K in string]: any } = {}
+> {
+  declare code: C;
+  declare message: string | undefined;
+  declare payload: Payload | undefined;
+  constructor(
+    code: C,
+    message?: string | undefined | null | Payload,
+    payload?: Payload | undefined | null
+  ) {
+    this.code = code;
+    if (payload) {
+      if (typeof message === "object" && message !== null) {
+        throw Error();
+      }
+      if (message != undefined) {
+        this.message = message;
+      }
+      this.payload = payload;
+    } else if (message != undefined) {
+      if (typeof message === "object") {
+        this.payload = message;
+      } else {
+        this.message = message;
+      }
+    } else {
+    }
+  }
+}
+
+interface CodeResultConstructor<
+  C extends number = number,
+  Payload extends { [K in string]: any } = {}
+> {
+  readonly prototype: CodeResultConstructor;
+  new (code: C, message?: string | Nullable): CodeResultConstructor;
+  new (code: C, payload?: Payload | Nullable): CodeResultConstructor;
+  new (
+    code: C,
+    message?: string | Nullable,
+    payload?: Payload | Nullable
+  ): CodeResultConstructor;
+}
+
+// export const CodeResult: CodeResultConstructor = CodeResultConstructor;
+export { CodeResultConstructor as CodeResult };
